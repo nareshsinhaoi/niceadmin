0446facf0f45f4bc2530ecda496138e46739215a
NARESH KUMAR SINHA <naresh@outlookindia.com>
2026-09-22 16:03:13 +0530

    2nd Updated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-//const { PrismaClient } = require('@/generated/prisma')
-import { PrismaClient } from './generated/prisma'
+// const { PrismaClient } = require('@/generated/prisma')
+// import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from '@prisma/client'
+
 const prisma = new PrismaClient()
 
 // A `main` function so that we can use async/await

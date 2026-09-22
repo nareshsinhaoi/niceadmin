8a57da6561b9289a3c4ce5485fc6e27053f97907
NARESH KUMAR SINHA <naresh@outlookindia.com>
2026-09-22 17:40:13 +0530

    7th Updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// const { PrismaClient } = require('@/generated/prisma')
+const { PrismaClient } = require('@/generated/prisma')
 // import { PrismaClient } from './generated/prisma'
 // import { PrismaClient } from '@prisma/client'
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
 
